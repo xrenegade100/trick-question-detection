88f68d0e7b30c8453cafee98d41c88a4a4697883
antgioia <antgioia10@gmail.com>
2022-07-19 12:00:06 +0200

    feat: add error control

---
+++ src/error/index.ts
@@ -0,0 +1,9 @@
+const ERRORS = {
+  EMPTY_STRING_PUNCTUATION:
+    "\n\n⛔ : Cannot remove punctuation from an empty string\n\n",
+  EMPTY_STRING_STEMMED: "\n\n⛔ : Cannot stem an empty string\n\n",
+  EMPTY_STRING_CHECKWORDS:
+    "\n\n⛔ : Cannot check if the sentence contains the words beacus it is empty\n\n",
+};
+
+export default ERRORS;
--- src/trickquestion/index.ts
+++ src/trickquestion/index.ts
@@ -1,45 +1,52 @@
 import Tagger from "wink-pos-tagger";
 const stem = require("wink-porter2-stemmer");
+import ERRORS from "../error/";
 
 /**
  * This function takes a string and removes all the punctuation and returns a string with only the words.
  * @param sentence The string to be cleaned.
  * @returns The cleaned string.
+ * @throws Will throw an error if the string is empty
  */
 const removePunctuation = (sentence: string) => {
-    const tagger = new Tagger();
+  if (sentence.trim().length === 0)
+    throw new Error(ERRORS.EMPTY_STRING_PUNCTUATION);
+  const tagger = new Tagger();
 
-    let sentNoPunctuation = (" " + sentence).slice(1);
+  let sentNoPunctuation = (" " + sentence).slice(1);
 
-    const pos = tagger.tagSentence(sentence);
+  const pos = tagger.tagSentence(sentence);
 
-    //in realtà si potrebbe migliorare se tipo togliamo la virgola una volta non controlliamo se ce ne sta un altra dopo
-    for (let i = 0; i < pos.length; i++) {
-        if (pos[i].tag === "punctuation") {
-            sentNoPunctuation = sentNoPunctuation.replaceAll(pos[i].value, "");
-        }
+  //in realtà si potrebbe migliorare se tipo togliamo la virgola una volta non controlliamo se ce ne sta un altra dopo
+  for (let i = 0; i < pos.length; i++) {
+    if (pos[i].tag === "punctuation") {
+      sentNoPunctuation = sentNoPunctuation.replaceAll(pos[i].value, "");
     }
+  }
 
-    return sentNoPunctuation;
+  return sentNoPunctuation;
 };
 
 /**
  * This function takes a string and stems all the words in it.
  * @param sentence The string to be stemmed.
  * @returns The stemmed string.
+ * @throws Will throw an error if the string is empty
  */
 const stemSentence = (sentence: string) => {
-    const words = sentence.split(" ");
-    let stemmedString = "";
+  if (sentence.trim().length === 0)
+    throw new Error(ERRORS.EMPTY_STRING_STEMMED);
+  const words = sentence.split(" ");
+  let stemmedString = "";
 
-    for (let i = 0; i < words.length; i++) {
-        if (i === 0) stemmedString += stem(words[i]) + " ";
-        else if (i !== 0 && i !== words.length - 1)
-            stemmedString += stem(words[i]) + " ";
-        else if (i === words.length - 1) stemmedString += stem(words[i]);
-    }
+  for (let i = 0; i < words.length; i++) {
+    if (i === 0) stemmedString += stem(words[i]) + " ";
+    else if (i !== 0 && i !== words.length - 1)
+      stemmedString += stem(words[i]) + " ";
+    else if (i === words.length - 1) stemmedString += stem(words[i]);
+  }
 
-    return stemmedString;
+  return stemmedString;
 };
 
 /**
@@ -48,8 +55,8 @@ const stemSentence = (sentence: string) => {
  * @returns The number of words in the string.
  */
 const countTokens = (sentence: string) => {
-    const sentTokens = sentence.split(" ");
-    return sentTokens.filter((parola) => parola !== "").length;
+  const sentTokens = sentence.split(" ");
+  return sentTokens.filter((parola) => parola !== "").length;
 };
 
 /**
@@ -57,12 +64,15 @@ const countTokens = (sentence: string) => {
  * This function takes a string and checks if it includes some common patterns
  * @param sentence The string to be checked.
  * @returns True if the string includes some common patterns, false otherwise.
+ * @throws Will throw an error if the string is empty
  */
 const checkWords = (sentence: string) => {
-    const WORDS = ["if you", "don't", "do not"];
-    const check = WORDS.find((word) => sentence.includes(word));
-    if (check) return true;
-    return false;
+  if (sentence.trim().length === 0)
+    throw new Error(ERRORS.EMPTY_STRING_CHECKWORDS);
+  const WORDS = ["if you", "don't", "do not"];
+  const check = WORDS.find((word) => sentence.includes(word));
+  if (check) return true;
+  return false;
 };
 
 /**
@@ -72,32 +82,32 @@ const checkWords = (sentence: string) => {
  * @returns The number of keywords in the string.
  */
 const countKeywords = (sentence: string) => {
-    let numKeywords = 0;
-    const keywords = [
-        "box",
-        "tick",
-        "uncheck",
-        "receive",
-        "email",
-        "newsletter",
-        "checkbox",
-        "please",
-        "discount",
-        "benefit",
-        "offer",
-        "promotion",
-        "receive",
-        "special offer",
-        "unsubscribe"
-    ];
+  let numKeywords = 0;
+  const keywords = [
+    "box",
+    "tick",
+    "uncheck",
+    "receive",
+    "email",
+    "newsletter",
+    "checkbox",
+    "please",
+    "discount",
+    "benefit",
+    "offer",
+    "promotion",
+    "receive",
+    "special offer",
+    "unsubscribe",
+  ];
 
-    keywords.forEach((element) => {
-        if (sentence.includes(stem(element))) {
-            numKeywords++;
-        }
-    });
+  keywords.forEach((element) => {
+    if (sentence.includes(stem(element))) {
+      numKeywords++;
+    }
+  });
 
-    return numKeywords;
+  return numKeywords;
 };
 
 /**
@@ -106,8 +116,8 @@ const countKeywords = (sentence: string) => {
  * @returns True if the string contains a comma, false otherwise.
  */
 const checkForComma = (sentence: string) => {
-    if (sentence.includes(",")) return true;
-    return false;
+  if (sentence.includes(",")) return true;
+  return false;
 };
 
 /**
@@ -116,36 +126,36 @@ const checkForComma = (sentence: string) => {
  * @returns A confidence score.
  */
 const isTrickQuestion = (sentence: string) => {
-    const SCORES = {
-        E1: 2,
-        E2: 2,
-        E3: 0.5,
-        E4: 1.5
-    };
-    let scoresTotal = 0;
+  const SCORES = {
+    E1: 2,
+    E2: 2,
+    E3: 0.5,
+    E4: 1.5,
+  };
+  let scoresTotal = 0;
 
-    Object.values(SCORES).forEach((score) => {
-        scoresTotal += score;
-    });
+  Object.values(SCORES).forEach((score) => {
+    scoresTotal += score;
+  });
 
-    let score = 0;
+  let score = 0;
 
-    const sentNoPunctuation = removePunctuation(sentence);
-    const stemmedSentence = stemSentence(sentNoPunctuation);
-    const numWordsInSentence = countTokens(stemmedSentence);
+  const sentNoPunctuation = removePunctuation(sentence);
+  const stemmedSentence = stemSentence(sentNoPunctuation);
+  const numWordsInSentence = countTokens(stemmedSentence);
 
-    if (numWordsInSentence >= 11) score += SCORES.E4;
+  if (numWordsInSentence >= 11) score += SCORES.E4;
 
-    if (checkWords(stemmedSentence)) score += SCORES.E1;
+  if (checkWords(stemmedSentence)) score += SCORES.E1;
 
-    if (countKeywords(stemmedSentence) >= 3) score += SCORES.E2;
+  if (countKeywords(stemmedSentence) >= 3) score += SCORES.E2;
 
-    if (checkForComma(sentence)) score += SCORES.E3;
+  if (checkForComma(sentence)) score += SCORES.E3;
 
-    return score / scoresTotal;
+  return score / scoresTotal;
 };
 
 const sentence =
-    "If you DON'T want to receive these emails, please tick this box.";
+  "If you DON'T want to receive these emails, please tick this box.";
 
 export default isTrickQuestion;
